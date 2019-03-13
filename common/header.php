6132feaa86dd4a9cb4b4aa3b64107355d63a085b
stephane piriou <stephane.piriou@zaclys.net>
2019-03-13 19:55:07 +0100

    Updated import

--- common/header.php
+++ common/header.php
@@ -10,64 +10,11 @@ echo '<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <title>test1</title>
-    <script language="JavaScript" type="text/javascript" src="../jqwidgets/jqx-all.js"></script>
-    <link rel="stylesheet" href="../bootstrap/bootstrap.css" >
-    <link rel="stylesheet" href="../jqwidgets/styles/jqx.base.css\">
-    <link rel="stylesheet" href="../jqwidgets/styles/jqx.bootstrap.css" >
-
-    <script type="text/javascript" src="../jquery/jquery.js"></script>
-    <script type="text/javascript" src="../bootstrap/bootstrap.js"></script>
-    <script type="text/javascript" src="../jqwidgets/jqxcore.js"></script>
-    <script type="text/javascript" src="../jqwidgets/jqxbuttongroup.js"></script>
-    <script type="text/javascript" src="../jqwidgets/jqxmenu.js"></script>
-    <script type="text/javascript" src="../jqwidgets/jqxbuttons.js"></script>
-    <script type="text/javascript" src="../scripts/demos.js"></script>
-
-    <style>
-        html,
-        body {
-            height: 100%;
-            /* The html and body elements cannot have any padding or margin. */
-        }
-        /* Wrapper for page content to push down footer */
-        #wrap {
-            min-height: 100%;
-            height: auto;
-            /* Negative indent footer by its height */
-            margin: 0 auto -60px;
-            /* Pad bottom by footer height */
-            padding: 0 0 60px;
-        }
-        /* Set the fixed height of the footer here */
-        #footer {
-            height: 60px;
-            background-color: #f5f5f5;
-        }
-        /* Not required for template or sticky footer method. */
-        #wrap > .container {
-            padding: 60px 15px 0;
-        }
-        .container .text-muted {
-            margin: 20px 0;
-        }
-        #footer > .container {
-            padding-left: 15px;
-            padding-right: 15px;
-        }
-        #navBar {
-            background: transparent !important;
-            border: none;
-            box-shadow: none;
-            -webkit-box-shadow: none;
-        }
-        .navbar {
-            min-height: 20px !important;
-        }
-        code {
-            font-size: 80%;
-        }
-    </style>
-
+     <link rel="stylesheet" href="../bootstrap/bootstrap.css" >
+    <link rel="stylesheet" href="../jqwidgets/styles/jqx.base.css">
+    <link rel="stylesheet" href="../jqwidgets/styles/jqx.energyblue.css">
+    <link rel="stylesheet" href="../styles/styles.css">
+    <script type="text/javascript" src="../jqwidgets/jqx-all.js"></script>
 </head>
 <body>
 
