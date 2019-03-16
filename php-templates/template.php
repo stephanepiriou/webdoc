edb677db1e3f373f89768ab1d821c3007e004257
stephane piriou <stephane.piriou@zaclys.net>
2019-03-16 09:53:02 +0100

    Initial commit

--- php-templates/template.php
+++ php-templates/template.php
@@ -1,8 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: stephane
- * Date: 2019-03-15
- * Time: 22:58
- */
+include('../common/header.php');
+include('../common/menu.php');
 ?>
+
+
+
+<?php
+echo '<div class="row">
+            <div class="col">
+                &nbsp;
+            </div>
+        </div>
+        <div class="row">
+            <!-- TITLE PAGE-->
+            <div id="page-title" class="offset-lg-3 col-lg-6">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <h5 style="text-align: center">Créer un type d\'individu</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                &nbsp;
+            </div>
+        </div>
+        <div class="row">
+            <div id="main-panel"  class="offset-lg-3 col-lg-6">
+                <div class="container">
+                    <div class="row">
+                        &nbsp;
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-12" id="footer">
+
+</div>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>';
+?>
+
+
