c45850a9585101e846c32f2a199fe3fa94c69515
stephane piriou <stephane.piriou@zaclys.net>
2019-06-10 19:23:35 +0200

    Adapted menu to mvc architecture

--- App/Views/Common/menu.php
+++ App/Views/Common/menu.php
@@ -58,36 +58,36 @@
                         </ul>
                     <li>Individu
                         <ul>
-                            <li><a href="../individus/chercher-type-individu.php">Chercher un Individu</a></li>
-                            <li><a href="../individus/creer-un-individu.php">Créer un Individu</a></li>
+                            <li><a href="/individus/search">Chercher un Individu</a></li>
+                            <li><a href="/individus/new">Créer un Individu</a></li>
                             <li style="color: rgb(148, 176,202);">---------------------</li>
-                            <li><a href="../individus/chercher-type-individu.php">Chercher un Type d'Individu</a></li>
-                            <li><a href="../individus/creer-un-individu.php">Créer un Type d'Individu</a></li>
+                            <li><a href="/types-individu/search">Chercher un Type d'Individu</a></li>
+                            <li><a href="/types-individu/new">Créer un Type d'Individu</a></li>
                         </ul>
                     </li>
                     <li>Documents
                         <ul>
-                            <li><a href="../documents/chercher-un-document.php">Chercher un Document</a></li>
+                            <li><a href="/documents/search">Chercher un Document</a></li>
                             <li style="color: rgb(148, 176,202);">---------------------</li>
-                            <li><a href="../documents/chercher-type-document.php">Chercher un Type de Document</a></li>
-                            <li><a href="../documents/creer-type-document.php">Créer un Type de Document</a></li>
+                            <li><a href="/types-document/search">Chercher un Type de Document</a></li>
+                            <li><a href="/types-document/new">Créer un Type de Document</a></li>
                         </ul>
                     </li>
                     <li>Backup
                         <ul>
-                            <li><a href="../backup/creer-un-backup.php">Créer un backup</a></li>
-                            <li><a href="../backup/restaurer-un-backup.php">Restaurer un backup</a></li>
+                            <li><a href="/backup/create">Créer un backup</a></li>
+                            <li><a href="/backup/restore">Restaurer un backup</a></li>
                         </ul>
                     </li>
                     <li>Utilisateurs
                         <ul>
-                            <li><a href="../utilisateurs/creer-un-utilisateur.php">Créer un utilisateur</a></li>
-                            <li><a href="../utilisateurs/chercher-un-utilisateur.php">Chercher un utilisateur</a></li>
+                            <li><a href="/users/search">Chercher un utilisateur</a></li>
+                            <li><a href="/users/new">Créer un utilisateur</a></li>
                         </ul>
-                    </li@   >
+                    </li>
                     <li>?
                         <ul>
-                            <li><a href="../aide/aide.php">Aide</a></li>
+                            <li><a href="/miscellaneous/help">Aide</a></li>
                             <li><a id="about" >Au sujet de...</a></li>
                         </ul>
                     </li>
