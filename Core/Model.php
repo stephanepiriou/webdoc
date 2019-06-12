43adce2c6ccf5c928f694348fafa098afe4cd4d9
stephane piriou <stephane.piriou@zaclys.net>
2019-06-12 20:14:16 +0200

    UTF-8 Collation

--- Core/Model.php
+++ Core/Model.php
@@ -13,8 +13,8 @@ abstract class Model
 
         if ($db === null){
             $dsn = 'mysql:host='.Config::DB_HOST.';dbname='.Config::DB_NAME.';charset=utf8';
-            $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
-
+            $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $db->exec('SET NAMES utf8');
             //Throw an exception when an error occurs
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
