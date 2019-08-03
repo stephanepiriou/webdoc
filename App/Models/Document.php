c95b578a694c89cccbf85e3a4367a4c21c5cda26
stephane piriou <stephane.piriou@zaclys.net>
2019-08-03 23:36:26 +0200

    Created listByIndividuId() Method

--- App/Models/Document.php
+++ App/Models/Document.php
@@ -24,9 +24,20 @@ class Document extends Model
     }
 
     /**
-     * Fetch all documents
+     * Fetch all documents by Individu and deliver it in an INNER JOIN
      */
-    public function list(){
-
+    public static function listByIndividuId($individuid){
+        $sql = 'SELECT documents.id AS document_id, documents.nom AS document_name, typesdocument.name AS type_document_name 
+                FROM documents 
+                INNER JOIN documents_individus ON documents.id=documents_individus.documentid 
+                INNER JOIN individus ON individus.id=documents_individus.individuid INNER JOIN typesdocument on documents.DocumentTypesid = typesdocument.id 
+                WHERE individuid=:individuid';
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':individuid', $individuid, PDO::PARAM_INT);
+        $stmt->execute();
+        $array = $stmt->fetchAll();
+        $jsonList = json_encode($array,JSON_UNESCAPED_UNICODE);
+        return $jsonList;
     }
 }
